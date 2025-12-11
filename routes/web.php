38c503b007622ff93f3956c4e9a7367fa84dae72
NovaRaii <pkoppany40@gmail.com>
2025-12-11 13:56:13 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -23,29 +23,41 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 
-// Authors
+// Authors - Public routes
 Route::get('authors', [AuthorController::class, 'index'])->name('authors.index');
-Route::get('authors/create', [AuthorController::class, 'create'])->name('authors.create');
-Route::post('authors', [AuthorController::class, 'store'])->name('authors.store');
 Route::get('authors/{author}', [AuthorController::class, 'show'])->name('authors.show');
-Route::get('authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit');
-Route::match(['put', 'patch'], 'authors/{author}', [AuthorController::class, 'update'])->name('authors.update');
-Route::delete('authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy');
 
-// Books
+// Authors - Protected routes (require authentication)
+Route::middleware('auth')->group(function () {
+    Route::get('authors/create', [AuthorController::class, 'create'])->name('authors.create');
+    Route::post('authors', [AuthorController::class, 'store'])->name('authors.store');
+    Route::get('authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit');
+    Route::match(['put', 'patch'], 'authors/{author}', [AuthorController::class, 'update'])->name('authors.update');
+    Route::delete('authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy');
+});
+
+// Books - Public routes
 Route::get('books', [BookController::class, 'index'])->name('books.index');
-Route::get('books/create', [BookController::class, 'create'])->name('books.create');
-Route::post('books', [BookController::class, 'store'])->name('books.store');
 Route::get('books/{book}', [BookController::class, 'show'])->name('books.show');
-Route::get('books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
-Route::match(['put', 'patch'], 'books/{book}', [BookController::class, 'update'])->name('books.update');
-Route::delete('books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
 
-// Categories
+// Books - Protected routes (require authentication)
+Route::middleware('auth')->group(function () {
+    Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+    Route::post('books', [BookController::class, 'store'])->name('books.store');
+    Route::get('books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+    Route::match(['put', 'patch'], 'books/{book}', [BookController::class, 'update'])->name('books.update');
+    Route::delete('books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+});
+
+// Categories - Public routes
 Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
 Route::get('categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
-Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-Route::match(['put', 'patch'], 'categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+// Categories - Protected routes (require authentication)
+Route::middleware('auth')->group(function () {
+    Route::get('categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::match(['put', 'patch'], 'categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+});
